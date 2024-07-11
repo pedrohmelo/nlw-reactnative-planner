510f8e08429b0ac1745fb4e4e45fbc250616cd0e
pedrohmelo <phmc_1995@hotmail.com>
2024-07-11 10:39:11 -0300

    class 01

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+//importação dos tokens para customizar o tema
+import { colors } from "./src/styles/colors";
+import { fontFamily } from "./src/styles/fontFamily";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: colors,
+      fontFamily: fontFamily,
+    },
+  },
+  plugins: [],
+};
